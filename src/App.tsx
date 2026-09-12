e1e193ca79aa92445d2f6f55a2d475e9dd84b514
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2026-09-12 18:56:42 +0530

    Project setup and ci pipeline learning

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import Counter from './counter/Counter'
+
+function App() {
+
+
+  return (
+    <>
+     <Counter/>
+    </>
+  )
+}
+
+export default App
